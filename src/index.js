09f4d1ea6d95053597bd20b162919e4fffe0bac4
UnaRumel <108756938+UnaRumel@users.noreply.github.com>
2023-05-11 16:00:53 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
-import 'modern-normalize/modern-normalize.css';
 import './index.css';
 
 ReactDOM.render(
